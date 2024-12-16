6668360c0c301fa03fceeebfae3f40aea7de8c64
Lee eo jin <djwls7777@gmail.com>
2024-12-16 09:40:33 +0900

    ver3

--- src/main/java/com/green/greengramver6/feed/FeedService.java
+++ src/main/java/com/green/greengramver6/feed/FeedService.java
@@ -120,6 +120,7 @@ public class FeedService {
         Map<Long, FeedCommentGetRes> commentHashMap = new HashMap<>();
         for(FeedCommentDto item : feedCommentList) {
             long feedId = item.getFeedId();
+
             if(!commentHashMap.containsKey(feedId)) {
                 FeedCommentGetRes feedCommentGetRes = new FeedCommentGetRes();
                 feedCommentGetRes.setCommentList(new ArrayList<>());
